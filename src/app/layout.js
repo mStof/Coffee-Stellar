d0f511d5a9291bedc257cf02b10734eb05cac032
Mateus <137577094+mStof@users.noreply.github.com>
2024-04-06 20:37:47 -0300

    correcting the path

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import { Lobster, Besley } from "next/font/google";
-import { SpeedInsights } from '@vercel/speed-insights'
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const lobster = Lobster({
   weight: "400",
